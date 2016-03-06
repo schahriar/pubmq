de4e4597144838ca41f605af240e9d509bf3c129
Schahriar SaffarShargh <info@schahriar.com>
2016-03-06 00:54:40 -0800

    [tests] Updated unit tests

--- tests/init.unit.test.js
+++ tests/init.unit.test.js
@@ -1,19 +1,30 @@
 // #1
 describe("Initialization Test Suite", function () {
-  this.timeout(10000);
+  this.timeout(5000);
   it("should start listening", function (done) {
     server.listen(18080);
     server.on("listening", done.bind(null, null));
   });
+  it("should ping/pong", function (done) {
+    var client = new PubMQ.Client(8087);
+    client.connect("localhost:18080");
+    client.ping();
+    client.on("pong", function (address) {
+      expect(address).to.have.property("port", "18080");
+      done();
+    });
+  });
   it("should pub/sub", function (done) {
-    var client1 = new PubMQ.Client("localhost:18080", 8090);
+    var client1 = new PubMQ.Client(8090);
+    client1.connect("localhost:18080");
     client1.subscribe("hello");
     client1.on(":hello", function (message) {
-      //expect(message.toString()).to.equal("hello world!");
+      expect(message.toString()).to.equal("hello world!");
       done();
     });
     setTimeout(function () {
-      var client2 = new PubMQ.Client("localhost:18080", 8092);
+      var client2 = new PubMQ.Client(8092);
+      client2.connect("localhost:18080");
       client2.publish("hello", "hello world!");
     }, 20);
   });
