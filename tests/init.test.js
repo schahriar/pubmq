a55575ceb3f978f5ec5fd3820a1c4da53f3adee3
Schahriar SaffarShargh <info@schahriar.com>
2016-03-06 19:22:00 -0800

    [tests] Updated E2E

--- tests/init.test.js
+++ tests/init.test.js
@@ -1,5 +1,4 @@
 // #1
-var client1;
 describe("Initialization Test Suite", function () {
   this.timeout(5000);
   it("should start listening", function (done) {
--- tests/sub.E2E.test.js
+++ tests/sub.E2E.test.js
@@ -14,6 +14,36 @@ describe("Subscribe E2E Test Suite", function () {
       done();
     });
   });
+  it("should unsubscribe", function () {
+    client1.unsubscribe("hello");
+  });
+  it("should receive up to max (10) queued messages", function (done) {
+    this.timeout(5000);
+    var max = 10;
+    var hits = 0;
+    // Buffer 5 over client max
+    for (var i = 0; i < max + 5; i++) {
+      client2.publish("hello", "NO#" + i);
+    }
+    function handler(message) {
+      hits++;
+      if (hits === max) {
+        // Wait 100ms to make sure we don't get extra packages
+        setTimeout(function () {
+          // Remove Listener
+          client1.removeListener(":hello", handler);
+          done();
+        }, 100);
+      } else if (hits > max) {
+        throw new Error("Received more messages than expected");
+      }
+    }
+    setTimeout(function () {
+      // Resubscribe
+      client1.on(":hello", handler);
+      client1.subscribe("hello");
+    }, 1000);
+  });
   it("should pub/sub", function (done) {
     client1.once(":hello", function (message) {
       expect(message.toString()).to.equal("hello world!");
